9d5b40dd28694426e22117e3780e7ef7daa83d9f
wybosys <wybosys@gmail.com>
2014-04-01 00:42:12 +0800

    prj

---
+++ ios/n2core.sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  n2core.sample
+//
+//  Created by wangyuanbo on 14-4-1.
+//  Copyright (c) 2014年 wybosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

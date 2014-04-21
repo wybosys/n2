28bc6b905d843352ed677254b5dbe1506451f9c9
wybosys <wybosys@gmail.com>
2014-04-21 15:28:06 +0800

    prj

---
+++ src/ui/N2List.h
@@ -0,0 +1,19 @@
+
+#ifndef __N2LIST_355DB85987D94F3E807CC8BB6A5FD4E5_H_INCLUDED
+#define __N2LIST_355DB85987D94F3E807CC8BB6A5FD4E5_H_INCLUDED
+
+N2UI_BEGIN
+
+class List
+: public ViewController
+{
+public:
+    
+    List();
+    virtual ~List();
+    
+};
+
+N2UI_END
+
+#endif
---
+++ src/ui/N2List.mm
@@ -0,0 +1,17 @@
+
+#import "N2Ui.h"
+#import "N2List.h"
+
+N2UI_BEGIN
+
+List::List()
+{
+    
+}
+
+List::~List()
+{
+    
+}
+
+N2UI_END

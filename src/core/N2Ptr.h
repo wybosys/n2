3ad00284f0b1259194192c8a43da6921d9d35a70
wybosys <wybosys@gmail.com>
2014-04-24 10:47:48 +0800

    prj

--- src/core/N2Ptr.h
+++ src/core/N2Ptr.h
@@ -228,69 +228,6 @@ public:
     
 };
 
-template <typename T>
-class PtrVector
-: protected ::std::vector<T*>
-{
-    typedef ::std::vector<T*> vector_t;
-    
-public:
-    
-    PtrVector()
-    {
-        
-    }
-    
-    ~PtrVector()
-    {
-        clear();
-    }
-    
-    using vector_t::begin;
-    using vector_t::end;
-    using vector_t::push_back;
-    
-    void clear()
-    {
-        for (auto i = begin(); i != end(); ++i)
-            delete *i;
-        vector_t::clear();
-    }
-    
-};
-
-template <typename K, typename T>
-class PtrMap
-: protected ::std::map<K, T*>
-{
-    typedef ::std::map<K, T*> map_t;
-    
-public:
-    
-    PtrMap()
-    {
-        
-    }
-    
-    ~PtrMap()
-    {
-        clear();
-    }
-    
-    using map_t::begin;
-    using map_t::end;
-    using map_t::find;
-    using map_t::operator [];
-    
-    void clear()
-    {
-        for (auto i = begin(); i != end(); ++i)
-            delete i->second;
-        map_t::clear();
-    }
-    
-};
-
 N2_END
 
 # endif

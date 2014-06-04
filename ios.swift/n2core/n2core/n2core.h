6787b2a39ba5aaa5df221174d62f3c2656626827
wybosys <wybosys@gmail.com>
2014-06-04 17:56:33 +0800

    swift

---
+++ ios.swift/n2core/n2core/n2core.h
@@ -0,0 +1,13 @@
+//
+//  n2core.h
+//  n2core
+//
+//  Created by wybo on 14-6-4.
+//  Copyright (c) 2014年 wybosys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface n2core : NSObject
+
+@end

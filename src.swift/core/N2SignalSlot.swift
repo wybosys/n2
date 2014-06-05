eae6a4c79a177537605b349c1a1ccb0745a38bc2
wybosys <wybosys@gmail.com>
2014-06-05 17:06:56 +0800

    swift

--- src.swift/core/N2SignalSlot.swift
+++ src.swift/core/N2SignalSlot.swift
@@ -16,12 +16,18 @@ class Signals : NSObject
     
 }
 
-protocol SignalSlotPattern
-{
-    
-}
+var __nsobj_dynamickey_signals: CConstVoidPointer?
 
-extension NSObject : SignalSlotPattern
+extension NSObject
 {
+    func signals() -> Signals
+    {
+        var ret:AnyObject = objc_getAssociatedObject(self, &__nsobj_dynamickey_signals)
+        if ret === nil {
+            ret = Signals()
+            objc_setAssociatedObject(self, &__nsobj_dynamickey_signals, ret, OBJC_ASSOCIATION_RETAIN_NONATOMIC.asUnsigned())
+        }
+        return ret as Signals
+    }
     
 }
